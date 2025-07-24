67328b3017e7abfae1ecf3b3877fb18dca3759fe
Jin Tao <j.tao@campus.tu-berlin.de>
2025-07-24 11:28:21 +0200

    initial push

---
+++ src/isaac_ros_visual_slam/isaac_ros_visual_slam/launch/isaac_only_realsense.launch.py
@@ -0,0 +1,49 @@
+# SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES
+# Copyright (c) 2021-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import launch
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    """Launch file which brings up only RealSense camera node with BearCar URDF."""
+    
+    realsense_camera_node = Node(
+        name='camera',
+        namespace='camera',
+        package='realsense2_camera',
+        executable='realsense2_camera_node',
+        parameters=[{
+                'enable_infra1': True,
+                'enable_infra2': True,
+                'enable_color': False,
+                'enable_depth': False,
+                'depth_module.emitter_enabled': 0,
+                'depth_module.profile': '640x360x90',
+                'enable_gyro': True,
+                'enable_accel': True,
+                'gyro_fps': 200,
+                'accel_fps': 200,
+                'unite_imu_method': 2,
+                'initial_reset': True
+        }]
+    )
+
+    return launch.LaunchDescription([realsense_camera_node])
